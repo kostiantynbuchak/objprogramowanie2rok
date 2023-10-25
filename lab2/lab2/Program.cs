300049997be86c4c5891a72104d7dfe6646c530c
kostiantynbuchak <148963561+kostiantynbuchak@users.noreply.github.com>
2023-10-25 13:19:17 +0200

    Add files via upload

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lab2.constructions;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = new Construction(23, 10, 4, 80, "brick");
+            Console.WriteLine(constr.BuildMaterial);
+        }
+    }
+}
---
+++ lab2/lab2/constructions/constructions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2.constructions
+{
+    class Construction
+    {
+        public Construction(float height,float width,int entrances, int humancapacity, string buildmaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humancapacity;
+            BuildMaterial = buildmaterial;
+        }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }        
+        public string BuildMaterial { get; set; }
+    }
+}
